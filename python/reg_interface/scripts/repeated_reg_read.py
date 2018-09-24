6cbb72f2518402baf7312f3b97ae46f6fb9aa9c6
Andrew Levin <andrew.michael.levin@cern.ch>
2018-09-24 10:50:03 +0200

    use format(), write to both a file and stdout

--- python/reg_interface/scripts/repeated_reg_read.py
+++ python/reg_interface/scripts/repeated_reg_read.py
@@ -56,9 +56,29 @@ if __name__ == '__main__':
     transaction_cnt_before = readAddress(getNode("GEM_AMC.SLOW_CONTROL.VFAT3.TRANSACTION_CNT").real_address)
     link_reset_before = readAddress(getNode("GEM_AMC.GEM_SYSTEM.CTRL.LINK_RESET").real_address)
 
-    print >> f, "|        | CRC_ERROR_CNT | PACKET_ERROR_CNT | BITSTUFFING_ERROR_CNT | TIMEOUT_ERROR_CNT | AXI_STROBE_ERROR_CNT | TRANSACTION_CNT |"
-    print >> f, "|:------ | :------------ | :--------------- | :-------------------- | :---------------- | :------------------- | :-------------- |"
-    print >> f, "| before | "+crc_error_cnt_before+"    | "+packet_error_cnt_before+"       | "+bitstuffing_error_cnt_before+"            | "+timeout_error_cnt_before+"        | "+axi_strobe_error_cnt_before+"           | "+transaction_cnt_before+"      |"
+    f.write( "|        | CRC_ERROR_CNT | PACKET_ERROR_CNT | BITSTUFFING_ERROR_CNT | TIMEOUT_ERROR_CNT | AXI_STROBE_ERROR_CNT | TRANSACTION_CNT |\n")
+    print( "|        | CRC_ERROR_CNT | PACKET_ERROR_CNT | BITSTUFFING_ERROR_CNT | TIMEOUT_ERROR_CNT | AXI_STROBE_ERROR_CNT | TRANSACTION_CNT |")
+    
+    f.write("|:------ | :------------ | :--------------- | :-------------------- | :---------------- | :------------------- | :-------------- |\n")
+    print("|:------ | :------------ | :--------------- | :-------------------- | :---------------- | :------------------- | :-------------- |")
+
+    f.write("| before | {0}  | {1} | {2} | {3} | {4} | {5} |\n".format(
+            crc_error_cnt_before,
+            packet_error_cnt_before,
+            bitstuffing_error_cnt_before,
+            timeout_error_cnt_before,
+            axi_strobe_error_cnt_before,
+            transaction_cnt_before
+         ))
+
+    print("| before | {0}  | {1} | {2} | {3} | {4} | {5} |".format(
+            crc_error_cnt_before,
+            packet_error_cnt_before,
+            bitstuffing_error_cnt_before,
+            timeout_error_cnt_before,
+            axi_strobe_error_cnt_before,
+            transaction_cnt_before
+         ))
 
     node = getNode(args.reg_name)
 
@@ -81,10 +101,48 @@ if __name__ == '__main__':
     axi_strobe_error_cnt_after = readAddress(getNode("GEM_AMC.SLOW_CONTROL.VFAT3.AXI_STROBE_ERROR_CNT").real_address)
     transaction_cnt_after = readAddress(getNode("GEM_AMC.SLOW_CONTROL.VFAT3.TRANSACTION_CNT").real_address)
 
-    print >> f, "| after  | "+crc_error_cnt_after+"    | "+packet_error_cnt_after+"       | "+bitstuffing_error_cnt_after+"            | "+timeout_error_cnt_after+"        | "+axi_strobe_error_cnt_after+"           | "+transaction_cnt_after+"      |"
+    crc_error_cnt_delta = str(int(crc_error_cnt_after,16) - int(crc_error_cnt_before,16))
+    packet_error_cnt_delta = str(int(packet_error_cnt_after,16) - int(packet_error_cnt_before,16))
+    bitstuffing_error_cnt_delta = str(int(bitstuffing_error_cnt_after,16) - int(bitstuffing_error_cnt_before,16))
+    timeout_error_cnt_delta = str(int(timeout_error_cnt_after,16) - int(timeout_error_cnt_before,16))
+    axi_strobe_error_cnt_delta = str(int(axi_strobe_error_cnt_after,16) - int(axi_strobe_error_cnt_before,16))
+    transaction_cnt_delta = str(int(transaction_cnt_after,16) - int(transaction_cnt_before,16))
 
-    print >> f, "| delta  | "+str(int(crc_error_cnt_after,16) - int(crc_error_cnt_before,16))+"             | "+str(int(packet_error_cnt_after,16) - int(packet_error_cnt_before,16))+"                | "+str(int(bitstuffing_error_cnt_after,16) - int(bitstuffing_error_cnt_before,16))+"                     | "+str(int(timeout_error_cnt_after,16) - int(timeout_error_cnt_before,16))+"                 | "+str(int(axi_strobe_error_cnt_after,16) - int(axi_strobe_error_cnt_before,16))+"               | "+str(int(transaction_cnt_after,16) - int(transaction_cnt_before,16))+"          |"    
+    f.write("| after | {0}  | {1} | {2} | {3} | {4} | {5} |\n".format(
+            crc_error_cnt_after,
+            packet_error_cnt_after,
+            bitstuffing_error_cnt_after,
+            timeout_error_cnt_after,
+            axi_strobe_error_cnt_after,
+            transaction_cnt_after
+         ))
     
-    print >> f, d
+    print("| after | {0}  | {1} | {2} | {3} | {4} | {5} |".format(
+            crc_error_cnt_after,
+            packet_error_cnt_after,
+            bitstuffing_error_cnt_after,
+            timeout_error_cnt_after,
+            axi_strobe_error_cnt_after,
+            transaction_cnt_after
+         ))
+
+    f.write("| delta | {0}  | {1} | {2} | {3} | {4} | {5} |\n".format(
+            crc_error_cnt_delta,
+            packet_error_cnt_delta,
+            bitstuffing_error_cnt_delta,
+            timeout_error_cnt_delta,
+            axi_strobe_error_cnt_delta,
+            transaction_cnt_delta
+         ))
     
-    #readAddress()
+    print("| delta | {0}  | {1} | {2} | {3} | {4} | {5} |".format(
+            crc_error_cnt_delta,
+            packet_error_cnt_delta,
+            bitstuffing_error_cnt_delta,
+            timeout_error_cnt_delta,
+            axi_strobe_error_cnt_delta,
+            transaction_cnt_delta
+         ))
+
+    f.write(str(d)+"\n")
+    print(str(d))
